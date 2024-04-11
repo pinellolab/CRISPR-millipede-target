887ab74aefd3f528437a9927ede1fd46765d04ac
doczmp <41808462+doczmp@users.noreply.github.com>
2024-04-11 13:38:12 -0400

    Update models_inputs.py

--- crispr_millipede/modelling/models_inputs.py
+++ crispr_millipede/modelling/models_inputs.py
@@ -107,6 +107,7 @@ class MillipedeModelSpecification:
     experiment_merge_strategy: MillipedeExperimentMergeStrategy
     cutoff_specification: MillipedeCutoffSpecification
     shrinkage_input: Union[MillipedeShrinkageInput, None] = None
+    S: float = 1.0 #S parameter
     
     def validate_merge_strategies(self, replicate_merge_strategy: MillipedeReplicateMergeStrategy, experiment_merge_strategy:MillipedeExperimentMergeStrategy):
         if experiment_merge_strategy == MillipedeExperimentMergeStrategy.SUM:
